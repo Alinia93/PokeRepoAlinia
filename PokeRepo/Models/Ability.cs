ebc4dc274ec0e7a7d97771b03a6dce39a4571876
emeliehensfelt <emelie046@gmail.com>
2024-02-07 13:51:31 +0100

    push

---
+++ PokeRepo/Models/Ability.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeRepo.Models
+{
+    public class Ability
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+    }
+}
