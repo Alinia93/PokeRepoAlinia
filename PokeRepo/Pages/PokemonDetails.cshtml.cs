1b50b479365fa45b87d7cdc66c00d5ca132118f6
Alinia93 <alinia93@gmail.com>
2024-02-08 11:50:05 +0100

    HEJ

--- PokeRepo/Pages/PokemonDetails.cshtml.cs
+++ PokeRepo/Pages/PokemonDetails.cshtml.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PokeRepo.API;
+using PokeRepo.Data;
 using PokeRepo.Models;
 
 namespace PokeRepo.Pages
 {
     public class PokemonDetailsModel : PageModel
     {
+        private readonly AppDbContext context;
         public PokemonRoot? Pokemon { get; set; }
 
         public async Task OnGet(string name)
@@ -15,6 +17,8 @@ namespace PokeRepo.Pages
 
                 PokemonRoot response = await new APIcaller().MakeCall<PokemonRoot>($"v2/pokemon/{name}");
                 Pokemon = response;
+                context.Add(response);
+                context.SaveChanges();
 
 
             }
